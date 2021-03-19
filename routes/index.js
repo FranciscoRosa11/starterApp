15067ae63d64dcfc03aba2f987f3409bde99ba8a
FranciscoRosa11 <a80749@alunos.uminho.pt>
2021-03-19 11:44:15 +0000

    coisa

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/register', function(req,res) {
+  res.render('register', {user: "", email: ""})
+})
+
+
+
+module.exports = router;
