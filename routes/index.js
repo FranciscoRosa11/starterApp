33292ba817a4428602ee477aa37b5b0b0c16db09
FranciscoRosa11 <a80749@alunos.uminho.pt>
2021-03-20 23:20:44 +0000

    login done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const {ensureAuthenticated} = require("../config/auth.js")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,8 +11,10 @@ router.get('/register', function(req,res) {
   res.render('register')
 })
 
-router.get('/dashboard',(req,res)=>{
-  res.render('dashboard');
+router.get('/dashboard',ensureAuthenticated,(req,res)=>{
+  res.render('dashboard', {
+    user: req.user
+  });
 })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/user.js")
 var bcrypt = require('bcrypt');
+const passport = require('passport');
 
 //login handle
 router.get('/login',(req,res)=>{
@@ -81,5 +82,9 @@ router.post('/login',(req,res,next)=>{
 
 //logout
 router.get('/logout',(req,res)=>{
- })
+  req.logout();
+  req.flash('success_msg','Now logged out');
+  res.redirect('/users/login');
+})
+
 module.exports  = router;
